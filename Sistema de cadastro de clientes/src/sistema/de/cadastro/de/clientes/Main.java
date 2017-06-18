38c7aa4f82d1248c6bd6ad7bf8d2d7f6bccd1867
Magno <Magno@Not-Magno>
2017-06-18 00:53:14 -0300

    Primeiro commit, criando projeto

---
+++ Sistema de cadastro de clientes/src/sistema/de/cadastro/de/clientes/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema.de.cadastro.de.clientes;
+
+/** Sistema de cadastro de clientes
+ *  disciplina de POO 18/06/2017
+ *  Arquivo main.
+ * @author Magno Carvalho
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //inicio do codigo.
+        
+    }
+    
+}
